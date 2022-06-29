80d49989bf0f0dabecb141d9aeed917d65c62716
zhonghaigang <18291878634@163.com>
2022-06-29 23:20:59 +0800

    SystemMapper中增加@Repository注解，消除引用的地方报错。

--- spring-cloud-system/src/main/java/com/yun/cloud/Mapper/SystemMapper.java
+++ spring-cloud-system/src/main/java/com/yun/cloud/Mapper/SystemMapper.java
@@ -3,8 +3,9 @@ package com.yun.cloud.Mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yun.cloud.entity.SysUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 @Mapper
 public interface SystemMapper extends BaseMapper<SysUser> {
 
